81f9d754976b4017704844f57fd1e330c9595d3e
zhongg1 <zhong.guanyi@gmail.com>
2012-12-03 10:06:51 +0200

    currentlocation.js added

---
+++ client/js/data/currentlocation.js
@@ -0,0 +1,33 @@
+if (navigator.geolocation) {
+     
+    var options = {
+        enableHighAccuracy: true,
+        maximumAge: 60000,
+        timeout: 45000
+    };
+ 
+    // successCallback
+    function successHandler (position) {
+		var map = Map.getMap();
+        var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+		//alert("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
+		var image = new google.maps.MarkerImage('images/pin.png');
+		var marker = new google.maps.Marker({
+			position:coords,
+			map: map.mapComponent,
+			title:"You are here!",
+			icon:image
+		}); 
+	}
+ 
+    // errorCallback
+    function errorHandler (error) {
+        alert('Attempt to get location failed: ' + error.message);
+    }
+ 
+    // Get the User Position
+    navigator.geolocation.getCurrentPosition(successHandler, errorHandler, options);
+}
+else {
+    alert("Sorry, your browser not support Geolocation API.");
+}
